5648046a74900ee16ead46da5cd746fc6725a8dd
Daniel Komisar <dkomisar@anaconda.com>
2022-02-25 15:23:33 -0500

    add garbage to get around buffering

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,10 @@ echo Delaying 5 minutes
 
 COUNT=300
 while [[ $COUNT != 0 ]]; do
-  echo $COUNT
+  echo Step $COUNT
+  for i in $(seq 100); do
+    echo LKJSDFLKSJDLKFJLKSDJFLKSJDFLKJSLKDFJLKSDJFLKJSDLKFJSKLDFJLKSDJFLKJSDLKFJSLKDFJKLSDJFLKSJDFL
+  done
   sleep 1
   COUNT=$(( COUNT - 1 ))
 done
